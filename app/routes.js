1dc1e53c9370ab02c384eb407b059f2b2d67d7e0
Unknown <gabriellima.silva96@gmail.com>
2018-07-28 21:59:08 -0300

    Adicionando middleware de autenticacao das rotas /app

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const authController = require('./controllers/authController');
+const dashboardController = require('./controllers/dashboardController');
+
+const authMiddleware = require('./middlewares/auth');
 
 const routes = express.Router();
 
@@ -13,4 +16,7 @@ routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);
+
+routes.use('/app', authMiddleware);
+routes.get('/app/dashboard', dashboardController.index);
 module.exports = routes;
